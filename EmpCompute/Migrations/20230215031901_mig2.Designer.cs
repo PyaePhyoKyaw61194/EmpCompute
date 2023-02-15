881628dd4d032c18b58e001803dac39d07f43b5b
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2023-02-15 12:24:42 +0900

    add: scafold

--- EmpCompute/Migrations/20230215031901_mig2.Designer.cs
+++ EmpCompute/Migrations/20230215031901_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmpCompute.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EmpCompute.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230215031901_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -110,7 +113,7 @@ namespace EmpCompute.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("employee", (string)null);
+                    b.ToTable("employee");
                 });
 
             modelBuilder.Entity("EmpCompute.Entity.PaymentRecord", b =>
@@ -192,7 +195,7 @@ namespace EmpCompute.Migrations
 
                     b.HasIndex("TaxYearId");
 
-                    b.ToTable("paymentrecord", (string)null);
+                    b.ToTable("paymentrecord");
                 });
 
             modelBuilder.Entity("EmpCompute.Entity.TaxYear", b =>
@@ -209,7 +212,7 @@ namespace EmpCompute.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("taxyear", (string)null);
+                    b.ToTable("taxyear");
                 });
 
             modelBuilder.Entity("EmpCompute.Entity.PaymentRecord", b =>
--- EmpCompute/Program.cs
+++ EmpCompute/Program.cs
@@ -3,25 +3,47 @@ using EmpCompute.Services;
 using EmpCompute.Services.Implementation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using Rotativa.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container. 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+/* builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>(); */
+
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.Configure<IdentityOptions>(options =>
+     {
+         //Default Password Settings
+         options.Password.RequireDigit = true;
+         options.Password.RequireLowercase = true;
+         options.Password.RequireNonAlphanumeric = false;
+         options.Password.RequireUppercase = true;
+         options.Password.RequiredLength = 6;
+         options.Password.RequiredUniqueChars = 1;
+         //Default Lockout settings
+         options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+         options.Lockout.MaxFailedAccessAttempts = 5;
+         options.Lockout.AllowedForNewUsers = true;
+     });
+
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IPayComputationService, PayComputationService>();
 builder.Services.AddScoped<INationalInsuranceContributionService, NationalInsuranceContributionService>();
 builder.Services.AddScoped<ITaxService, TaxService>();
-RotativaConfiguration.Setup((Microsoft.AspNetCore.Hosting.IHostingEnvironment)builder.Environment);
+builder.Services.AddMvc();
+/* RotativaConfiguration.Setup((Microsoft.AspNetCore.Hosting.IHostingEnvironment)builder.Environment);
 
-
+ */
 var app = builder.Build();
+
 
 
 // Configure the HTTP request pipeline.
@@ -37,11 +59,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+/* app.UseAuthorization(); */
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
 
